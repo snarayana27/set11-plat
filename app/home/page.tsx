dc441ed38c44e7935bd883207111d2763b403a43
Santhosh Narayana <santhoshlal@gmail.com>
2024-03-02 19:00:43 +0000

    message

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,9 +12,9 @@ export default function HomePage() {
         className="w-48"
       />
       <h1 className="text-white">
-        Edit this page on{" "}
+        Coming soon{" "}
         <InlineSnippet className="ml-2 bg-blue-900 text-blue-100">
-          app/home/page.tsx
+          set11
         </InlineSnippet>
       </h1>
     </div>
